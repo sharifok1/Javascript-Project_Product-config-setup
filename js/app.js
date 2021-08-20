5a03ebe03a6dc4222ffd4e4155bbd4948e202953
sharifok1 <sharifok1@gmail.com>
2021-08-21 05:17:46 +0600

    javascript primary stage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,57 @@
-console.log('connected');
+function updateCost(isDefult,isPaid,is512GB,is1TB,isDelivery,id){
+    if(isDefult){
+        const productId = document.getElementById(id);
+        productId.innerText = 0;
+    }
+    else if(isPaid){
+        const extraMemoryCost = document.getElementById(id);
+        extraMemoryCost.innerText = 180;
+    }
+    else if(is512GB){
+        const extraMemoryCost = document.getElementById(id);
+        extraMemoryCost.innerText = 100;
+    }
+    else if(is1TB){
+        const extraMemoryCost = document.getElementById(id);
+        extraMemoryCost.innerText = 180;
+    }
+    else{
+        const extraMemoryCost = document.getElementById(id);
+        extraMemoryCost.innerText = 20;
+    }
+}
+//******************freeMemory(8GB) click handeler
+const freeMemory =document.getElementById('free-memory');
+freeMemory.addEventListener('click',function(){
+   const defultMemory = updateCost(true,false,false,false,false,'memory-cost')
+})
+//******************16GB Memory click handeler
+const paidMemory =document.getElementById('paid-memory');
+paidMemory.addEventListener('click',function(){
+    const updateMemory = updateCost(false,true,false,false,false,'memory-cost')
+})
+//******************free storage(256GB) click handeler
+const freeStorage =document.getElementById('free-storage');
+freeStorage.addEventListener('click',function(){
+    const defultStorage =updateCost(true,false,false,false,false,'storage-cost')
+})
+//******************512GB storage click handeler
+const paidStorage1 =document.getElementById('paid-storage1');
+paidStorage1.addEventListener('click',function(){
+    const updateStorage1 =updateCost(false,false,true,false,false,'storage-cost')
+})
+//******************1TB storage click handeler
+const paidStorage2 =document.getElementById('paid-storage2');
+paidStorage2.addEventListener('click',function(){
+    const updateStorage2 =updateCost(false,false,false,true,false,'storage-cost')
+})
+//******************free delivery click handeler
+const freeDelivery =document.getElementById('free-delivery');
+freeDelivery.addEventListener('click',function(){
+    const freeDelivery = updateCost(true,false,false,false,false,'delivery-cost')
+})
+//******************paid delivery click handeler
+const paidDelivery =document.getElementById('paid-delivery');
+paidDelivery.addEventListener('click',function(){
+    const paidDelivery = updateCost(false,false,false,false,true,'delivery-cost')
+})
